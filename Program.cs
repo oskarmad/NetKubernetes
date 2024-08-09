e3d82f67cee1af246d28f7801234c1a3fa721803
Oscar Mauricio Diaz <oskarmad@gmail.com>
2024-08-09 17:29:38 -0500

    Documentación endpoints con swagger
    Documentación endpoints con swagger

--- Program.cs
+++ Program.cs
@@ -1,11 +1,85 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NetKubernetes.Data;
+using NetKubernetes.Data.Inmuebles;
+using NetKubernetes.Data.Usuarios;
 using NetKubernetes.Middleware;
+using NetKubernetes.Models;
+using NetKubernetes.Profiles;
+using NetKubernetes.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.LogTo(Console.WriteLine, new[] {
+        DbLoggerCategory.Database.Command.Name},
+        LogLevel.Information).EnableSensitiveDataLogging();
+
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")!);
+});
+
+builder.Services.AddScoped<IInmuebleRepository, InmuebleRepository>();
+
+
 // Add services to the container.
+
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new InmuebleProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
+var builderSecurity = builder.Services.AddIdentityCore<Usuario>();
+var identityBuilder = new IdentityBuilder(builderSecurity.UserType, builder.Services);
+identityBuilder.AddEntityFrameworkStores<AppDbContext>();
+identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+builder.Services.AddSingleton<ISystemClock, SystemClock>();
+builder.Services.AddScoped<IJwtGenerador, JwtGenerador>();
+builder.Services.AddScoped<IUsuarioSesion, UsuarioSesion>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false
+                    };
+
+                });
+
+
+builder.Services.AddCors(o => o.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 
 var app = builder.Build();
 
@@ -15,34 +89,40 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseMiddleware<ManagerMiddleware>();
+
 app.UseAuthentication();
-// app.UseHttpsRedirection();
+app.UseCors("corsapp");
+//app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
+app.UseAuthorization();
 
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
+app.MapControllers();
+
+
+using (IServiceScope ambiente = app.Services.CreateScope())
+{
+    var services = ambiente.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<Usuario>>();
+        var context = services.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+        await LoadDatabase.InsertarData(context, userManager);
+
+    }
+    catch (Exception e)
+    {
+        var logging = services.GetRequiredService<ILogger<Program>>();
+        logging.LogError(e, "Ocurrio un error en la migracion");
+    }
+}
+
+
+
+
+
+
 
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
